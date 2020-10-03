257e1478f3d62d544733185eac1776fe5414a685
fisherek15 <fisherek15@gmail.com>
2020-10-03 20:10:46 +0200

    Basic about Streams

--- src/best/programming/Main.java
+++ src/best/programming/Main.java
@@ -1,8 +1,54 @@
 package best.programming;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        List<String> someBingoNumbers = Arrays.asList(
+                "N40", "N36",
+                "B12", "B6",
+                "G53", "G49", "G60", "G50", "g64",
+                "I26", "I17", "I29",
+                "O71"
+        );
+
+        List<String> gNumbers = new ArrayList<>();
+
+
+        someBingoNumbers.forEach(number -> {
+            if(number.toUpperCase().startsWith("G")) {
+                gNumbers.add(number);
+            }
+        });
+
+        gNumbers.sort((String s1, String s2) -> s1.compareTo(s2));
+        gNumbers.forEach((String s) -> System.out.println(s));
+
+        System.out.println("---------------");
+
+        List<String> someBingoNumbers2 = Arrays.asList(
+                "N40", "N36",
+                "B12", "B6",
+                "G53", "G49", "G60", "G50", "g64",
+                "I26", "I17", "I29",
+                "O71"
+        );
+
+        someBingoNumbers2.forEach(number -> {
+            System.out.println(number);
+        });
+
+        System.out.println("----");
+
+        someBingoNumbers2
+                .stream()
+                .map(String::toUpperCase)
+                .filter(s -> s.startsWith("G"))
+                .sorted()
+                .forEach(System.out::println);
     }
 }
