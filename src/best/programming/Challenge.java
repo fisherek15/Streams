2b1231af06f9690c653f301531ff826c741a134d
fisherek15 <fisherek15@gmail.com>
2020-10-06 22:32:47 +0200

    Challenge #5, #6, #7

--- src/best/programming/Challenge.java
+++ src/best/programming/Challenge.java
@@ -1,7 +1,11 @@
 package best.programming;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Function;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 public class Challenge {
 
@@ -32,6 +36,48 @@ public class Challenge {
         Supplier<String> iLoveJava = () -> "I love Java!";
         System.out.println(iLoveJava.get());
         // End of #4
+
+        // #5 Sort and print the words with large first letter
+        List<String> topName2015 = Arrays.asList(
+                "Amelia",
+                "Olivia",
+                "emily",
+                "Isla",
+                "Ava",
+                "oliver",
+                "Jack",
+                "Charlie",
+                "harry",
+                "Jacob"
+        );
+
+        List<String> firstUpperCaseList = new ArrayList<>();
+        topName2015.forEach(name ->
+                firstUpperCaseList.add(name.substring(0, 1).toUpperCase() + name.substring(1)));
+        //firstUpperCaseList.sort((s1, s2) -> s1.compareTo(s2));
+        //firstUpperCaseList.forEach(s -> System.out.println(s));
+        firstUpperCaseList.sort(String::compareTo);
+        firstUpperCaseList.forEach(System.out::println);
+        // End of #5
+
+        // #6 Do the same like in #5 but use stream
+            List<String> firstUpperCaseList2 = topName2015
+                    .stream()
+                    .map(name -> name.substring(0, 1).toUpperCase() + name.substring(1))
+                    .sorted()
+                    .collect(Collectors.toList());
+
+            firstUpperCaseList2.forEach(System.out::println);
+        // End of #6
+
+        // #7 Print count of names that start with 'A' and do it with stream
+        long countNamesStartWithA = topName2015
+                .stream()
+                .map(name -> name.substring(0, 1).toUpperCase() + name.substring(1))
+                .filter(s -> s.startsWith("A"))
+                .count();
+        System.out.println(countNamesStartWithA);
+        // End of #7
     }
 
     // #2 Write the following methods as a lambda expression
